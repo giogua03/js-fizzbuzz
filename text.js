26489d7fcda323f5757213c576c1d25b12964f44
giogua03 <giorgiogua03@gmail.com>
2022-10-24 16:48:48 +0200

    corretto max

--- text.js
+++ text.js
@@ -1,4 +1,10 @@
 const eleSquares = document.querySelector('.squares');
 
-for (let i = 0; i <= maxNumber; i++) {
-    eleSquares.innerHTML += `<div class="sq">${i}</div>`}
+for (let i = 0; i <= 100; i++) {
+    eleSquares.innerHTML += `<div class="sq">${i}</div>`
+}
+if (i % 3 == 0 ) {
+     eleSquares.innerHTML += `<div class="sq">{"fizz"}</div>`
+}else {
+        eleSquares.innerHTML += `<div class="sq">${i}</div>`
+    }
