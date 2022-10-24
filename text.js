17872c4e8d376c36d6eec19eba146f20644fb895
giogua03 <giorgiogua03@gmail.com>
2022-10-24 17:30:35 +0200

    finito

--- text.js
+++ text.js
@@ -1,11 +1,14 @@
 const eleSquares = document.querySelector('.squares');
 let i
 
-for (let i = 0; i <= 100; i++) {
-    eleSquares.innerHTML += `<div class="sq">${i}</div>`
-}
-if (i % 3 == 0 ) {
-     eleSquares.innerHTML += `<div class="sq">${"fizz"}</div>`
-}else {
-        eleSquares.innerHTML += `<div class="sq">${i}</div>`
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0  ) {
+        eleSquares.innerHTML += `<div class="sq4">${"FizzBuzz"}</div>`
+    }if (i % 3 == 0 && i % 5 != 0  ) {
+            eleSquares.innerHTML += `<div class="sq2">${"Fizz"}</div>`
+    }if (i % 3 != 0 && i % 5 == 0  ) {
+            eleSquares.innerHTML += `<div class="sq3">${"Buzz"}</div>`
+    }else if (i % 3 != 0 && i % 5 != 0  ) {
+            eleSquares.innerHTML += `<div class="sq">${i}</div>`
     }
+}
