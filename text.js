dc318895c8857c401c821f6bd6f9c626842ffe12
giogua03 <giorgiogua03@gmail.com>
2022-10-24 16:50:42 +0200

    corretto if

--- text.js
+++ text.js
@@ -4,7 +4,7 @@ for (let i = 0; i <= 100; i++) {
     eleSquares.innerHTML += `<div class="sq">${i}</div>`
 }
 if (i % 3 == 0 ) {
-     eleSquares.innerHTML += `<div class="sq">{"fizz"}</div>`
+     eleSquares.innerHTML += `<div class="sq">${"fizz"}</div>`
 }else {
         eleSquares.innerHTML += `<div class="sq">${i}</div>`
     }
